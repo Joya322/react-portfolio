f2e3a055c508ee071780c153c1ba7b0d0372d339
Jannatul Ferdoush <jannatferdoush1212@gmail.com>
2026-07-02 23:58:30 +0600

    resume added to view and download

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,19 +4,26 @@ const Home = () => {
       <div className="flex flex-col md:flex-row justify-center md:justify-between gap-10 items-center">
         <div id="home-content" className="md:w-2/4 order-2 md:order-1">
           <h1 className="text-5xl font-black">
-            Hello, I'm <br /> <span className="text-violet-700">Jannatul Ferdoush</span>
+            Hello, I'm <br />{" "}
+            <span className="text-violet-700">Jannatul Ferdoush</span>
           </h1>
+          <h4 className="text-xl font-semibold text-gray-700 mt-3">
+            Computer Science Lecturer | Full-Stack Web Developer (MERN)
+          </h4>
           <p className="mt-5 text-justify">
-            Motivated junior frontend developer skilled in HTML, CSS, Bootstrap
-            5, Tailwind CSS, JavaScript and React. Passionate about creating
-            responsive, user-friendly web applications with modern UI
-            frameworks. Eager to contribute to a dynamic team and grow as a
-            professional developer
+            I build modern, responsive, and user-friendly web applications using
+            the MERN Stack while sharing knowledge as a Computer Science
+            lecturer.
           </p>
 
-          <button className="btn px-5 py-3 bg-violet-600 text-white rounded-lg mt-6 hover:bg-violet-800">
+          <a
+            href="./Jannatul-Ferdoush-Resume.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="btn px-5 py-3 bg-violet-600 text-white rounded-lg mt-6 hover:bg-violet-800"
+          >
             Download CV
-          </button>
+          </a>
         </div>
         <div className="md:w-1/4 order-1 md:order-2">
           <img
