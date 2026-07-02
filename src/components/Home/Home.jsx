d6f1a17b636baf542be6f9350a4eea953a4b9c4e
Jannatul Ferdoush <jannatferdoush1212@gmail.com>
2026-07-03 00:36:40 +0600

    Social medias added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,37 +1,75 @@
+import { FaFacebook, FaGithub, FaLinkedin } from "react-icons/fa";
+
 const Home = () => {
   return (
-    <div className="">
-      <div className="flex flex-col md:flex-row justify-center md:justify-between gap-10 items-center">
-        <div id="home-content" className="md:w-2/4 order-2 md:order-1">
-          <h1 className="text-5xl font-black">
-            Hello, I'm <br />{" "}
-            <span className="text-violet-700">Jannatul Ferdoush</span>
-          </h1>
-          <h4 className="text-xl font-semibold text-gray-700 mt-3">
-            Computer Science Lecturer | Full-Stack Web Developer (MERN)
-          </h4>
-          <p className="mt-5 text-justify">
-            I build modern, responsive, and user-friendly web applications using
-            the MERN Stack while sharing knowledge as a Computer Science
-            lecturer.
-          </p>
+    <div className="flex flex-col md:flex-row justify-center md:justify-between gap-10 items-center mt-8">
+      <div id="home-content" className="md:w-2/4 order-2 md:order-1">
+        {/* Introduction */}
+        <h1 className="text-5xl font-black">
+          Hello, I'm <br />
+          <span className="text-violet-700">Jannatul Ferdoush</span>
+        </h1>
 
+        {/* Designation */}
+        <h4 className="text-xl font-semibold text-gray-700 my-6">
+          Computer Science Lecturer | Full-Stack Web Developer (
+          <span className="text-violet-700">MERN Stack</span>)
+        </h4>
+
+        {/* Summary */}
+        <p className="text-justify">
+          I build modern, responsive, and user-friendly web applications using
+          the MERN Stack while sharing knowledge as a Computer Science lecturer.
+        </p>
+
+        {/* Download Resume Button */}
+        <a
+          href="./Jannatul-Ferdoush-Resume.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="btn px-5 py-3 bg-violet-600 text-white rounded-lg my-6 hover:bg-violet-800"
+        >
+          Download Resume
+        </a>
+
+        {/* Social Links */}
+        <div className="flex gap-4">
+          {/* GitHub */}
           <a
-            href="./Jannatul-Ferdoush-Resume.pdf"
+            href="https://github.com/Joya322"
             target="_blank"
             rel="noopener noreferrer"
-            className="btn px-5 py-3 bg-violet-600 text-white rounded-lg mt-6 hover:bg-violet-800"
           >
-            Download CV
+            <FaGithub size={25} />
+          </a>
+
+          {/* LinkedIn */}
+          <a
+            href="https://www.linkedin.com/in/jannat-98y/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedin className="text-blue-700" size={25} />
+          </a>
+
+          {/* Facebook */}
+          <a
+            href="https://www.facebook.com/jannat.ferdoush.96"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaFacebook className="text-blue-700" size={25} />
           </a>
         </div>
-        <div className="md:w-1/4 order-1 md:order-2">
-          <img
-            className="rounded-full w-full"
-            src="./Jannat-WithoutBg.png"
-            alt=""
-          />
-        </div>
+      </div>
+
+      {/* Profile Image */}
+      <div className="md:w-1/4 mx-auto order-1 md:order-2">
+        <img
+          className="rounded-full w-full"
+          src="./Jannat-WithoutBg.png"
+          alt=""
+        />
       </div>
     </div>
   );
